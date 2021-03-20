92223b5b3068f13969f8740bec25b11628bc99fd
jandonggun <donggontrannguyen@gmail.com>
2021-03-20 10:34:41 +0700

    overlay

--- 3.easyBank/app/js/script.js
+++ 3.easyBank/app/js/script.js
@@ -1,5 +1,17 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 btnHamburger.addEventListener("click", (e) => {
   e.preventDefault();
-  btnHamburger.classList.toggle("open");
+  if (header.classList.contains("open")) {
+    // remove popup
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {
+    // open popup
+    header.classList.add("open");
+    overlay.classList.add("fade-in");
+    overlay.classList.remove("fade-out");
+  }
 });
