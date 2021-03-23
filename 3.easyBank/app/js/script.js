c090a4b3ade16362152c2537831911d9c44581f6
jandonggun <donggontrannguyen@gmail.com>
2021-03-23 19:06:43 +0700

    add nav mobile

--- 3.easyBank/app/js/script.js
+++ 3.easyBank/app/js/script.js
@@ -1,6 +1,8 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const navMobile = document.querySelector(".header__nav");
+const body = document.querySelector("body");
 btnHamburger.addEventListener("click", (e) => {
   e.preventDefault();
   if (header.classList.contains("open")) {
@@ -8,10 +10,20 @@ btnHamburger.addEventListener("click", (e) => {
     header.classList.remove("open");
     overlay.classList.remove("fade-in");
     overlay.classList.add("fade-out");
+
+    // NAV
+    navMobile.classList.remove("fade-in");
+    navMobile.classList.add("fade-out");
+    body.classList.remove("noscroll");
   } else {
     // open popup
     header.classList.add("open");
     overlay.classList.add("fade-in");
     overlay.classList.remove("fade-out");
+
+    // NAV
+    navMobile.classList.add("fade-in");
+    navMobile.classList.remove("fade-out");
+    body.classList.add("noscroll");
   }
 });
